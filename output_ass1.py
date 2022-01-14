a3cf2a73064bb88ddcd862146b43ebec5da6a1dd
NamanGoyal2048 <96754164+NamanGoyal2048@users.noreply.github.com>
2022-01-14 15:33:02 +0530

    Add files via upload

---
+++ output_ass1.py
@@ -0,0 +1,43 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 22:22:05) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:\Users\DELL\AppData\Local\Programs\Python\Python37\assignment1_NamanGoyal_21105015_ECE.py 
+            First question
+
+Enter the first number= 89
+Enter the second number= 98
+Enter the second number= 90
+Average of three numbers = 92.33333333333333
+
+
+
+               Second question
+
+Enter your Gross Income=50000
+Enter the number of dependents=4
+Your Taxable Income is = 28000.0
+Tax Amount =  5600.0
+
+
+
+              Third question
+
+Enter your Student ID: 21105015
+Enter your Name: Naman Goyal
+Enter your Gender out of "F","M" or "U":M
+Enter your branch:ECE
+Enter your cgpa:9.0
+[21105015, 'Naman Goyal', 'M', 'ECE', 9.0]
+
+
+
+             Fourth question
+
+Marks of five studenst: [90, 89, 72, 65, 57]
+sorted list of marks: [57, 65, 72, 89, 90]
+             Fifth question
+
+The list of colors: ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+Updated colors list: ['Red', 'Green', 'White', 'Pink', 'Yellow']
+Updated colors list: ['Red', 'Green', 'White', 'Purple', 'Pink']
+>>> 
